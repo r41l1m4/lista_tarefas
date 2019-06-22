0f6212bbd8277fd6a96fb1a8d2528683d4cd639d
Raí <rai.lima17@live.com>
2019-06-21 22:36:38 -0300

    impl remocao items

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,9 @@ class _HomeState extends State<Home> {
 
   List _toDoList = [];
 
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
   @override
   void initState() {
     super.initState();
@@ -109,6 +112,30 @@ class _HomeState extends State<Home> {
           });
         },
       ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+          
+          final snack = SnackBar(
+              content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                },
+            ),
+            duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
