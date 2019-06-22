f9525d94cdf2d3780ca76446f944cbbbc294c3ad
Raí <rai.lima17@live.com>
2019-06-21 21:43:54 -0300

    Inicio do layout.

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,41 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   List _toDoList = [];
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "Nova Tarefa",
+                    labelStyle: TextStyle(color: Colors.blueAccent),
+                  ),
+                )),
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  child: Text("ADD"),
+                  textColor: Colors.white,
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Future<File> _getFile() async {
@@ -39,10 +68,8 @@ class _HomeState extends State<Home> {
     try {
       final file = await _getFile();
       return file.readAsString();
-    }catch (e) {
+    } catch (e) {
       return null;
     }
   }
-
 }
-
