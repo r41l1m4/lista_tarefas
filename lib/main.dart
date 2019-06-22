12bec512fd0b9579dd7df91872c80aed01d2e0fc
Raí <rai.lima17@live.com>
2019-06-21 22:06:10 -0300

    checklist layout

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,19 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List _toDoList = ["rai", "raul"];
+  final _toDoController = TextEditingController();
+
+  List _toDoList = [];
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +46,7 @@ class _HomeState extends State<Home> {
               children: <Widget>[
                 Expanded(
                     child: TextField(
+                  controller: _toDoController,
                   decoration: InputDecoration(
                     labelText: "Nova Tarefa",
                     labelStyle: TextStyle(color: Colors.blueAccent),
@@ -43,7 +56,7 @@ class _HomeState extends State<Home> {
                   color: Colors.blueAccent,
                   child: Text("ADD"),
                   textColor: Colors.white,
-                  onPressed: () {},
+                  onPressed: _addToDo,
                 )
               ],
             ),
@@ -54,11 +67,16 @@ class _HomeState extends State<Home> {
               itemCount: _toDoList.length,
               itemBuilder: (context, index) {
                 return CheckboxListTile(
-                  title: Text(_toDoList[index]),
+                  title: Text(_toDoList[index]["title"]),
                   value: _toDoList[index]["ok"],
                   secondary: CircleAvatar(
-                    child: Icon(_toDoList[index] ? Icons.check : Icons.error),
+                    child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
                   ),
+                  onChanged: (checked) {
+                    setState(() {
+                      _toDoList[index]["ok"] = checked;
+                    });
+                  },
                 );
               },
             ),
